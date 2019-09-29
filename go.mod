bd0037b60e3a162196edf82a9dbf0a6c113266a6
Anurag Patel <anuragpatel.optnio@gmail.com>
2019-09-29 13:04:21 +0530

    rate limiter based on ip address

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/geekfarmer/ipratelimit
+
+go 1.12
+
+require golang.org/x/time v0.0.0-20190921001708-c4c64cad1fd0
---
+++ ipratelimit.go
@@ -0,0 +1,70 @@
+package ipratelimit
+
+import (
+	"net/http"
+	"sync"
+
+	"golang.org/x/time/rate"
+)
+
+// Limiter is used to rate-limit http-request for user's IP address
+type Limiter struct {
+	ips map[string]*rate.Limiter
+	mu  *sync.RWMutex
+	r   rate.Limit
+	b   int
+}
+
+// New returns a Limiter that will limit to the given RPS.
+func New(r rate.Limit, b int) *Limiter {
+	i := &Limiter{
+		ips: make(map[string]*rate.Limiter),
+		mu:  &sync.RWMutex{},
+		r:   r,
+		b:   b,
+	}
+
+	return i
+}
+
+// AddIP creates a new rate limiter and adds it to the ips map,
+// using the IP address as the key
+func (i *Limiter) AddIP(ip string) *rate.Limiter {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	limiter := rate.NewLimiter(i.r, i.b)
+
+	i.ips[ip] = limiter
+
+	return limiter
+}
+
+// GetLimiter returns the rate limiter for the provided IP address if it exists.
+// Otherwise calls AddIP to add IP address to the map
+func (i *Limiter) GetLimiter(ip string) *rate.Limiter {
+	i.mu.Lock()
+	limiter, exists := i.ips[ip]
+
+	if !exists {
+		i.mu.Unlock()
+		return i.AddIP(ip)
+	}
+
+	i.mu.Unlock()
+
+	return limiter
+}
+
+// IPRateLimit .
+func (i *Limiter) IPRateLimit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		limiter := i.GetLimiter(r.RemoteAddr)
+		if !limiter.Allow() {
+			http.Error(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
